9b30496e3e69fc9368879ec029317ff486b1da4a
Jamshidbeck <jamshidabdullajonov19@gmail.com>
2022-07-16 20:39:48 +0500

    ewdwed

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./Footer.css"
+import Facebook from "../../asets/Footer/facebook.png"
+import Instagram from "../../asets/Footer/instagram.png"
+import Twitter from "../../asets/Footer/twitter.png"
+// import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function Footer() {
   return (
-    <div>
-        <h1>Footer page</h1>
+    <div className='footer'>
+      <h1><u>Let’s Get in Touch !</u></h1>
+      <p className='footer-first-p'>Have any questions about our products? Looking to partner with us or inquire about a custom loral arrangament? Click the link below and shoot us an email!</p>
+      <div className="footer-email">
+        <p>SUNDAYANDSAGECO@GMAIL.COM</p></div>
+      <p className='footer-second-p'>We look forward to working with you and answering any questions you have!</p>
+      <h2>FOLLOW US:</h2>
+      <div className="footer-icons">
+      
+      </div>
     </div>
   );
 }
