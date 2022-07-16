78001bb398697f95cb713ad7d52bd79b52b826e3
Boburbek0101 <boburolimjonov0121@gmail.com>
2022-07-16 20:31:12 +0500

    1 va 2 sectionni bitirdim

--- src/pages/Section2/Section2.js
+++ src/pages/Section2/Section2.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './section2.css'
 
 export default function Section2() {
   return (
     <div>
-        <h1>Section2</h1>
+        <div className="Section2">
+      
+          <div className="Section2_div">
+          <h1>Why Shop With Us?</h1>
+            <span>
+              Sunday + Sage is a small, female-owned business based in Denver, Colorado.
+              During spring and summer, we function as a sustainable floral business. During the fall and winter
+              , we aim to support and partner with small/local businesses by developing curated gift boxes.
+              </span>
+
+          </div>
+        </div>
     </div>
   );
 }
