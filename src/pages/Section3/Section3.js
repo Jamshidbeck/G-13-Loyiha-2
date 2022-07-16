234b67ee12b116188e63e0d94968d4ba6f2c3fdb
AzimjonKing <azimjonjalilov1301@gmail.ru>
2022-07-16 19:35:44 +0300

    asets ichiga rasm

--- src/pages/Section3/Section3.js
+++ src/pages/Section3/Section3.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import "../Section3/section3.css"
 
 export default function Section3() {
   return (
-    <div>
-        <h1>Section 3</h1>
+    <div className="section3">
+
+      <div className="header">
+
+        <div className="heading">
+          <span></span>
+          <p>Floral Arrangements</p>
+          <span></span>
+        </div>
+
+        <span>Our floral arrangements are available during the spring/summer seasons. The flowers included in our bouquets change weekly based on our local farm supply, so be sure to check back often for different arrangements!</span>
+
+      </div>
+
+      <div className="box-container">
+        <div className="box">
+          
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
