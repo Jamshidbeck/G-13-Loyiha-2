02a58bbbe880603dce470c7304df67f8a614ab2e
Jamshidbeck <jamshidabdullajonov@gmail.com>
2022-07-19 14:35:12 +0500

    npm install react-icons --save ni install qiling

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import "./Footer.css"
-// import Facebook from "../../asets/Footer/facebook.png"
-// import Instagram from "../../asets/Footer/instagram.png"
-// import Twitter from "../../asets/Footer/twitter.png"
-// import FacebookIcon from '@mui/icons-material/Facebook';
+import {FaFacebook} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
+import {FaTwitter} from "react-icons/fa"
 
 export default function Footer() {
   return (
     <div className='footer'>
-      <h1><u>Let’s Get in Touch !</u></h1>
+      <h1>Let’s Get in Touch !</h1>
       <p className='footer-first-p'>Have any questions about our products? Looking to partner with us or inquire about a custom loral arrangament? Click the link below and shoot us an email!</p>
       <div className="footer-email">
         <p>SUNDAYANDSAGECO@GMAIL.COM</p></div>
       <p className='footer-second-p'>We look forward to working with you and answering any questions you have!</p>
       <h2>FOLLOW US:</h2>
-      <div className="footer-icons">
-      
+      <div className="footer-icons-div">
+        <FaFacebook className='footer-icons'/>
+        <FaInstagram className='footer-icons'/>
+        <FaTwitter className='footer-icons' />
       </div>
     </div>
   );
